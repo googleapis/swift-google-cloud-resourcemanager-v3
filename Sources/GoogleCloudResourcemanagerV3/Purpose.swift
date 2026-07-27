8e331204b7c03fd419dac3df9cdfcc4559d90551
Carlos O'Ryan <coryan@google.com>
2026-07-27 18:20:35 -0400

    chore: update librarian circa 2026-07-27 (#48)
    In this version, librarian generates correct cross-reference links for
    enums. This fixes a number of documentation warnings for
    `google-cloud-wkt` and many generated libraries.

--- Sources/GoogleCloudResourcemanagerV3/Purpose.swift
+++ Sources/GoogleCloudResourcemanagerV3/Purpose.swift
@@ -80,7 +80,7 @@ public enum Purpose: Codable, Equatable, Sendable {
 
   /// Initialize from a string value.
   ///
-  /// If the value is unknown, this initializes to ``.unknownStringValue(_:)``.
+  /// If the value is unknown, this initializes to [`unknownStringValue`](doc:Purpose/unknownStringValue(_:)).
   public init(stringValue: Swift.String) {
     switch stringValue {
     case "PURPOSE_UNSPECIFIED": self = .unspecified
@@ -91,7 +91,7 @@ public enum Purpose: Codable, Equatable, Sendable {
 
   /// Initialize from an integer value.
   ///
-  /// If the value is unknown, this initializes to ``.unknownIntValue(_:)``.
+  /// If the value is unknown, this initializes to [`unknownIntValue`](doc:Purpose/unknownIntValue(_:)).
   public init(intValue: Int) {
     switch intValue {
     case 0: self = .unspecified
