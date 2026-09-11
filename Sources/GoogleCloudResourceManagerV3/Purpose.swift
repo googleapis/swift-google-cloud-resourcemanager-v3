81f4cb33033adc301dd89afde9d32442108370e2
Carlos O'Ryan <coryan@google.com>
2026-09-11 20:11:51 +0000

    feat: update librarian with serialize-enums-as-strings (#829)
    Update librarian to use strings when serializing enums. Fixed a number
    of unit / integration tests.

--- Sources/GoogleCloudResourceManagerV3/Purpose.swift
+++ Sources/GoogleCloudResourceManagerV3/Purpose.swift
@@ -121,8 +121,8 @@ public enum Purpose: Codable, Equatable, Sendable {
   public func encode(to encoder: Encoder) throws {
     var container = encoder.singleValueContainer()
     switch self {
-    case .unspecified: return try container.encode(0)
-    case .gceFirewall: return try container.encode(1)
+    case .unspecified: return try container.encode("PURPOSE_UNSPECIFIED")
+    case .gceFirewall: return try container.encode("GCE_FIREWALL")
     case .unknownIntValue(let v): return try container.encode(v)
     case .unknownStringValue(let v): return try container.encode(v)
     }
